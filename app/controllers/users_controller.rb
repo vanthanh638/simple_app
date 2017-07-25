4602c38b98aefa933e39678b16443dd9dd05cb15
tranthanh <vanthanh638x11@gmail.com>
2017-07-25 15:25:13 +0700

    Use SSL and the Puma webserver in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 # User controller
 class UsersController < ApplicationController
-  def new; end
+  def show
+    # find user
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = I18n.t("users.new.flash")
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
 end
