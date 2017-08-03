56e57fb48d07b9959de308f35d54e37cdc450c79
tranthanh <vanthanh638x11@gmail.com>
2017-08-03 15:50:27 +0700

    Add account activation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  def hello
-    render html: "hello, word!"
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "users.edit.login"
+    redirect_to login_url
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.try(:admin?)
   end
 end
