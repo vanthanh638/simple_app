dc7d7708b89f1bdf9c6da14d405397479748e564
tranthanh <vanthanh638x11@gmail.com>
2017-08-03 15:50:27 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 # User controller
 class UsersController < ApplicationController
-  def show
-    # find user
-    @user = User.find_by id: params[:id]
-    if @user.nil?
-      flash[:danger] = t("users.new.user_nil")
-      redirect_to signup_path
-    else
-      flash.now[:success] = t("users.new.flash")
-    end
+  before_action :load_user, only: %i(show edit update destroy)
+  before_action :logged_in_user, only: %i(index edit update)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -19,11 +19,30 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t("users.new.flash")
+      flash[:success] = t "users.new.flash"
       redirect_to @user
     else
       render :new
     end
+  end
+
+  def edit; end
+
+  # update user
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "users.edit.update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  # Delete user
+  def destroy
+    @user.destroy
+    flash[:success] = t "users.index.delete_success"
+    redirect_to users_url
   end
 
   private
@@ -31,5 +50,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  # load user
+  def load_user
+    # find user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "users.new.user_nil"
+    redirect_to signup_path
   end
 end
